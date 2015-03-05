8fdd0afd17746066161d4989b1f89395222631cc
aghosh47 <aghosh@kreeti.com>
2015-03-05 15:45:52 +0530

    ui changes

--- views/details.jsx
+++ views/details.jsx
@@ -18,7 +18,7 @@ var Repo = React.createClass({
     var repo = this.props.repository;
     return (
       <div className = "row">
-        <div className = "col-xs-6">
+        <div className = "col-xs-6 repo-name">
           <a href={repo.html_url}>{repo.name}</a>
         </div>
         <div className = "col-xs-6">
@@ -33,15 +33,31 @@ var Details = React.createClass({
   getInitialState : function() {
     return {
       img_url: '',
+      details: '',
       repos: [],
       pages: []
     }
   },
 
-  fetchUserDetails : function() {
+  fetchUserDetails: function() {
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.github.com/users/' + this.props.user.username ,
+      dataType: 'json'
+    }).done(function(data, sc,$xhr) {
+      this.setState({
+        details: {username: data.login, html_url: data.html_url, name: data.name, email: data.email, followers: data.followers, following: data.following, repos: data.public_repos, gists: data.public_gists}
+      })
+    }.bind(this))
+  },
+
+  fetchRepoDetails : function() {
     var regex = /(?:page=)(\d+)[^\w]+(?:rel=)"([^"]+)"/g,
     page = this.props.page,
     pages = {};
+
+    this.fetchUserDetails();
+
     $.ajax({
       type: 'GET',
       url: 'https://api.github.com/users/' + this.props.user.username + '/repos',
@@ -56,7 +72,7 @@ var Details = React.createClass({
           }
         }
         this.setState({
-          img_url: data[0].owner.avatar_url + "&size=" + Math.ceil(window.innerHeight/2),
+          img_url: data[0].owner.avatar_url + "&size=100",
           repos: data.map(function(el) { return { id: el.id, html_url: el.html_url, name: el.name, owner: data[0].owner.login } }),
           pages: pages
         });
@@ -66,12 +82,12 @@ var Details = React.createClass({
 
 
   componentDidMount: function() {
-    this.fetchUserDetails();
+    this.fetchRepoDetails();
   },
 
   componentDidUpdate: function(prevProps, prevState) {
     if(prevProps.page !== this.props.page)
-      this.fetchUserDetails();
+      this.fetchRepoDetails();
   },
 
   render: function() {
@@ -83,34 +99,40 @@ var Details = React.createClass({
     });
 
     pages = _.map(this.state.pages, function(data, i) {
-      return <Paginate page = { data } nate = { i } username = { username } onFetch = { self.fetchUserDetails } />
+      return <Paginate key = {i} page = { data } nate = { i } username = { username } onFetch = { self.fetchRepoDetails } />
     });
 
     return (
       <div className = "top-container">
         <Header />
+        <div className = "card-container">
+          <div className="card">
+            <div className="cover">
+            </div>
+            <div className="user">
+              <img src={this.state.img_url} alt="gravatar" width="75" height="75"/>
+              <div className="details">
+                <div><small> {this.state.details.email} </small></div>
+                <div><small><span className="badge">{ this.state.details.followers}</span> Followers</small></div>
+                <div><small><span className="badge">{ this.state.details.following}</span> Following </small></div>
+              </div>
+            </div>
+            <div className="user-name">
+              <div className="user-name"> { this.state.details.name } <small className="details"> {username} </small> </div>
+            </div>
+          </div>
+        </div>
         <div className = "container">
-          <div className = "col-xs-12 cover">
-            <div className = "row">
-              <div className = "col-xs-6">
-                <a href = "#" className =  "thumbnail"> <UserImage src = { this.state.img_url }/> </a>
-              </div>
-              <div className = "col-xs-6">
-                <div className = "page-header">
-                  <h4> { username } </h4>
-                </div>
+          <div className="row">
+            <div className = "col-xs-12 navigation">
+              <div className = "row">
+                { pages }
               </div>
             </div>
-          </div>
 
-          <div className = "col-xs-12 navigation">
-            <div className = "row">
-              { pages }
+            <div className = "col-xs-12">
+              { repos }
             </div>
-          </div>
-
-          <div className = "col-xs-12">
-            { repos }
           </div>
         </div>
       </div>
